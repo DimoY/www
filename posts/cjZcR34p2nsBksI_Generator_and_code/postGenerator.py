c92b40c5734d0489caf6a915b211a27f03498d81
dimo <dimo.yordanov@proton.me>
2023-12-03 10:59:26 +0200

    automate index.html + language support+ python geb

---
+++ posts/cjZcR34p2nsBksI_Generator_and_code/postGenerator.py
@@ -0,0 +1,2 @@
+##If you stumble on this, this is the code that is used to generate every blog post
+
--- posts/postGenerator.py
+++
@@ -1,2 +0,0 @@
-##If you stuble on this, this is the code that is used to generate every blog post
-
